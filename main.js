63c0db97747cbb43dfa0fd32500b8c506ac26f6e
dinitrogen <noshowdon@gmail.com>
2022-06-24 12:15:04 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("Finance App testing"),(()=>{const t=document.querySelector("#content"),e=document.createElement("div");e.setAttribute("class","textDiv"),e.textContent="This is the landing page!",t.appendChild(e)})()})();
